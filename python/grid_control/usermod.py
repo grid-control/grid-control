379594b0ac856c994618351202511dba6d96b097
Andreas Oehler <andreasoehler@gmx.net>
2007-02-09 16:17:16 +0000

    usermod...
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@30 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/usermod.py
+++ python/grid_control/usermod.py
@@ -4,19 +4,26 @@ from grid_control import Module
 class UserMod(Module):
 	def __init__(self, config):
 		Module.__init__(self, config)
-
+		self.config = config
 		self.executable = config.getPath('UserMod', 'executable')
-		self.inFiles = config.get('UserMod', 'input files').split()
-		self.outFiles = config.get('UserMod', 'output files').split()
-
-
+		self.inFiles = config.get('UserMod', 'inputfiles').split()
+		self.outFiles = config.get('UserMod', 'outputfiles').split()
+		self.userParameters = config.get('UserMod', 'userparameters')
+		
+	def getSoftwareMembers(self):
+		return ('0')
+		
 	def getInFiles(self):
 		return self.inFiles
-
-
+			
+			
 	def getOutFiles(self):
 		return self.outFiles
-
-
+			
+			
 	def getJobArguments(self, job):
 		return "%d %s" % (job, os.path.basename(self.executable))
+
+	def getExeParameter(self)
+	        return self.userParameters
+			
