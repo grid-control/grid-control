4e8ffbaa9295893718ea2caeef2eb6bcf0b1cfcb
Fred Stober <stober@cern.ch>
2009-04-15 13:25:58 +0000

    style
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@235 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/user_mod.py
+++ python/grid_control/user_mod.py
@@ -4,17 +4,18 @@ from grid_control import Module
 class UserMod(Module):
 	def __init__(self, config, init):
 		Module.__init__(self, config, init)
-		self.executable = config.getPath('UserMod', 'executable')
-		self.arguments = config.get('UserMod', 'arguments')
-
-
-	def getCommand(self):
-		return 'chmod u+x %s; ./%s "$@"' % (os.path.basename(self.executable), os.path.basename(self.executable))
+		self._executable = config.getPath('UserMod', 'executable')
+		self._arguments = config.get('UserMod', 'arguments')
 
 
 	def getInFiles(self):
-		return Module.getInFiles(self) + [ self.executable ]
+		return Module.getInFiles(self) + [ self._executable ]
+
+
+	def getCommand(self):
+		cmd = os.path.basename(self._executable)
+		return 'chmod u+x %s; ./%s "$@"' % (cmd, cmd)
 
 
 	def getJobArguments(self, job):
-		return self.arguments
+		return self._arguments
