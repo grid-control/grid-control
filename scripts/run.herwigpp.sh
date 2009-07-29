902055992e4cb08fdef3f6090b5d4bd8f9aafca0
Fred Stober <stober@cern.ch>
2009-07-29 12:51:11 +0000

    comments
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@443 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/run.herwigpp.sh
+++ scripts/run.herwigpp.sh
@@ -4,6 +4,8 @@ echo "HERWIG++ module starting"
 echo
 echo "---------------------------"
 
+# This script uses the standalone Herwig++ which is distributed
+# together with CMSSW ... so first find the CMSSW directory
 if [ -z "$VO_CMS_SW_DIR" -a -d "/wlcg/sw/cms/experimental" ]; then
 	echo "[WLCG-SITE] Using $VO_CMS_SW_DIR"
 elif [ -z "$VO_CMS_SW_DIR" -a -d "/wlcg/sw/cms" ]; then
@@ -19,6 +21,7 @@ fi
 
 export SW_CMSSW_EXT="$VO_CMS_SW_DIR/slc4_ia32_gcc345/external"
 
+# Dir shortcuts to all Herwig++ dependencies
 export SW_HERWIGPP="$SW_CMSSW_EXT/herwigpp/2.3.2-cms"
 export SW_THEPEG="$SW_CMSSW_EXT/thepeg/1.4.2"
 export SW_LHA="$SW_CMSSW_EXT/lhapdf/5.6.0-cms2"
@@ -26,6 +29,7 @@ export SW_GSL="$SW_CMSSW_EXT/gsl/1.10-cms"
 export SW_GCCLIB="$SW_CMSSW_EXT/gcc/3.4.5-CMS19"
 export SW_HEPMC="$SW_CMSSW_EXT/hepmc/2.03.06-cms3"
 
+# Set all the necessary lib paths for Herwig++ 
 export ThePEG_INSTALL_PATH="$SW_THEPEG/lib/ThePEG"
 export PATH="$PATH:$SW_THEPEG/bin:$SW_HERWIGPP/bin:$SW_LHA/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SW_THEPEG/lib/ThePEG:$SW_HERWIGPP/lib/Herwig++"
@@ -42,8 +46,10 @@ for FILE in $SUBST_FILES; do
 	echo "Running over file $FILE"
 	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	cat $FILE
+	echo
 
 	Herwig++ read $FILE
 	Herwig++ run *.run $HW_PARA || exit $?
 	rm *.run
 done
+echo
