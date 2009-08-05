6f261746d1695662628fa7078672e93fd38ad026
Matthias Wolf <matthias.wolf.60@nd.edu>
2009-08-05 13:08:40 +0000

    Fix Typo
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@486 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/job_db.py
+++ python/grid_control/job_db.py
@@ -105,7 +105,7 @@ class JobDB:
 			print "(error code: %d - %s)" % (jobObj.get('retcode'), jobObj.get('dest'))
 		elif (state == Job.QUEUED) and jobObj.get('dest') != 'N/A':
 			print "(%s)" % jobObj.get('dest')
-		elif (state in [Job.WAITING, Job.ABORTED, JOB.DISABLED]) and jobObj.get('reason'):
+		elif (state in [Job.WAITING, Job.ABORTED, Job.DISABLED]) and jobObj.get('reason'):
 			print '(%s)' % jobObj.get('reason')
 		elif (state == Job.SUCCESS) and jobObj.get('runtime'):
 			print "(runtime %s)" % utils.strTime(jobObj.get('runtime'))
