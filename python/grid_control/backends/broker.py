a861085bdafde51f10de7fdeea2b28d4e3027a3f
Matthias Wolf <matthias.wolf.60@nd.edu>
2009-10-16 20:40:59 +0000

    Rework SimpleBroker to submit into the shortest queue possible.
    Results may not always be optimal, but this should work for the IC1 and comparable.  If the user specifies queues, their order should be obeyed.
    
    
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@573 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/backends/broker.py
+++ python/grid_control/backends/broker.py
@@ -11,9 +11,11 @@ class Broker(AbstractObject):
 	def matchQueue(self, reqs):
 		return reqs
 
-	def addQueueReq(self, reqs, queues):
+	def addQueueReq(self, reqs, queues, randomize=False):
 		if len(queues) > 0:
-			rIdx = random.randint(0, len(queues) - 1)
+			rIdx = 0
+			if randomize:
+				rIdx = random.randint(0, len(queues) - 1)
 			reqs.append((WMS.SITES, queues[rIdx]))
 
 
@@ -22,7 +24,7 @@ class DummyBroker(Broker):
 		Broker.__init__(self, config, queues)
 
 	def matchQueue(self, reqs):
-		self.addQueueReq(reqs, self.userQueue)
+		self.addQueueReq(reqs, self.userQueue, True)
 		return reqs
 
 
@@ -31,6 +33,11 @@ class SimpleBroker(Broker):
 		Broker.__init__(self, config, queues)
 
 	def matchQueue(self, reqs):
+		def item(index):
+			def helper(lst):
+				return lst[index]
+			return helper
+
 		def matcher(props):
 			for key, value in reqs:
 				if key not in props:
@@ -39,15 +46,32 @@ class SimpleBroker(Broker):
 					return False
 			return True
 
+		def queue_cmp(a, b):
+			diffs = set()
+			for key in set(a.keys() + b.keys()):
+				if key in a and key in b:
+					diffs.add(cmp(a[key], b[key]))
+				elif key in a:
+					diffs.add(-1)
+				else:
+					diffs.add(1)
+			if len(diffs) > 1:
+				return 0
+			return diffs.pop()
+
+		sorted_queues = sorted(self.queues.items(), queue_cmp, item(1))
+
 		if self.queues:
-			match = dict(filter(lambda (x, y): matcher(y), self.queues.items())).keys()
+			match = map(item(0), (filter(lambda (x, y): matcher(y), sorted_queues)))
 			if len(match) == 0:
 				match = self.userQueue
 		else:
 			match = self.userQueue
 
 		if len(self.userQueue) > 0:
-			self.addQueueReq(reqs, filter(lambda x: x in self.userQueue, match))
+			# Submit job with the first queue that matches the requirements
+			# and the user specified in the configuration file
+			self.addQueueReq(reqs, filter(lambda x: x in match, self.userQueue))
 		else:
 			self.addQueueReq(reqs, match)
 		return reqs
