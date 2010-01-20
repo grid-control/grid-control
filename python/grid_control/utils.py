d32d00a65b37ee4147649f5aef04657b41c5ac96
Fred Stober <stober@cern.ch>
2010-01-20 19:21:21 +0000

    option splitter #1
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@644 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/utils.py
+++ python/grid_control/utils.py
@@ -42,6 +42,30 @@ def sorted(list, comp=None, key=None):
 	return tmp
 
 
+def optSplit(opt, prefixList):
+	""" Split option strings into fixed tuples
+	>>> optSplit("abc:ghi#def", ["#", ":"])
+	('abc', 'def', 'ghi')
+	>>> optSplit("abcghi#def", ["#", ":"])
+	('abcghi', 'def', '')
+	"""
+	firstMatch = filter(lambda x: x >= 0, map(lambda p: opt.find(p), prefixList))
+	if len(firstMatch) > 0 and min(firstMatch) > -1:
+		first = opt[:min(firstMatch)]
+	else:
+		first = opt[:]
+	def pSubstring(p):
+		start = opt.find(p)
+		if start == -1:
+			return ''
+		posList = map(lambda n: opt.find(n, start + 1), filter(lambda n: n != p, prefixList))
+		end = None
+		if len(prefixList) > 1 and min(posList) > -1:
+			end = min(posList)
+		return opt[start + 1:end]
+	return tuple([first] + map(pSubstring, prefixList))
+
+
 def unique(lst):
 	return dict(map(lambda x: (x, None), lst)).keys()
 
