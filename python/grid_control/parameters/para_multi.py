f0d01b133f842c8e3143f8696d835d3f9f55cb3c
Matthias Wolf <matthias.wolf.60@nd.edu>
2010-02-02 23:57:07 +0000

    Allow for parameter type in UberParaMod.
    A range can be specified by
            parameters = ... foo ...
            foo = range(1000)
            foo type = expr
    now.  Other types split strings on linefeeds only or every whitespace.
    
    
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@657 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/parameters/para_multi.py
+++ python/grid_control/parameters/para_multi.py
@@ -1,5 +1,5 @@
 from para_base import ParaMod
-from grid_control import utils
+from grid_control import exceptions, utils
 
 class UberParaMod(ParaMod):
 	"""This module builds all possible combinations of parameters
@@ -12,7 +12,17 @@ class UberParaMod(ParaMod):
 	gives the following parameter combinations of (spam, ham, egg):
 
 		(A, 1, 2), (B, 1, 2), (A, 3, 4), (B, 3, 4)
-	"""
+
+	Values of single parameters (e.g. "spam" in the example above) can
+	either be "words", span "lines" or be an "expr"ession that gets
+	evaluated.  To let "foo" go from 0 to 9, the following can be used:
+
+		paramters = foo
+		foo       = range(10)
+		foo type  = expr
+
+	The default type is "words" - the parameter value gets split on
+	whitespace."""
 	def __init__(self, config):
 		ParaMod.__init__(self, config)
 
@@ -23,6 +33,28 @@ class UberParaMod(ParaMod):
 		options = dict(map(lambda (o, v): (repr(option_to_tuple(o)),
 			utils.parseTuples(v)), config.parser.items('ParaMod')))
 
+		def expandParam(param, type='words'):
+			"""Expand param depending on its type.
+
+			>>> expandParam("Where the wild things grow")
+			['Where', 'the', 'wild', 'things', 'grow']
+			
+			>>> expandParam("range(3)", "expr")
+			[0, 1, 2]
+			
+			Possible improvements: enum values to indicate type;  doctest
+			might be wrong (unable to test)"""
+			actions = {
+				'expr': eval,
+				'lines': lambda l: map(str.strip, l.splitlines()),
+				'words': lambda l: map(str.strip, l.split())
+			}
+
+			try:
+				return actions[type](param)
+			except:
+				raise ConfigError("Illegal type '%s' in parameter expansion" % type)
+
 		self.pars = {}
 		for p in names:
 			if isinstance(p, tuple):
@@ -32,8 +64,8 @@ class UberParaMod(ParaMod):
 				else:
 					self.pars[repr(p)] = []
 			else:
-				self.pars[repr(p)] = map(str.strip,
-					config.get('ParaMod', p).split())
+				self.pars[repr(p)] = expandParam(config.get('ParaMod', p),
+						config.get('ParaMod', '%s type' % p, default='words'))
 
 	def getParams(self):
 		res = [[]]
