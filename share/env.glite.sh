1d7c9ea4c93058daf607a6344c3b2e0c603cd6bd
Manuel Zeise <zeise@cern.ch>
2010-02-12 17:31:24 +0000

    Revert VO variables to previous values after sourcing UI
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@670 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- share/env.glite.sh
+++ share/env.glite.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 echo "Searching for gLite environment..."
-if [ -z "$GLITE_LOCATION" -a -d "/wlcg/sw/UI_glite-3_1" ]; then
-	source "/wlcg/sw/UI_glite-3_1/external/etc/profile.d/grid-env.sh"
-	echo "[WLCG-SITE] Using gLite `glite-version`"
-elif [ -z "$GLITE_LOCATION" -a -d "/afs/desy.de/project/glite" ]; then
-	source "/afs/desy.de/project/glite/UI/etc/profile.d/grid-env.sh"
-	echo "[AFS-SITE] Using gLite `glite-version`"
+
+if [ -z "$GLITE_LOCATION" ]; then
+	# Save local VO environment variables
+	VO_KEEPER="$MY_LANDINGZONE/env.glite.old"
+	VO_REVERT="$MY_LANDINGZONE/env.glite.new"
+	export | grep VO_ | sed -e "s/^.*VO_/VO_/" > "$VO_KEEPER"
+
+	cat $VO_KEEPER
+	# Source UI
+	if [ -d "/wlcg/sw/UI_glite-3_1" ]; then
+		source "/wlcg/sw/UI_glite-3_1/external/etc/profile.d/grid-env.sh"
+		echo "[WLCG-SITE] Using gLite `glite-version`"
+	elif [ -d "/afs/desy.de/project/glite" ]; then
+		source "/afs/desy.de/project/glite/UI/etc/profile.d/grid-env.sh"
+		echo "[AFS-SITE] Using gLite `glite-version`"
+	fi
+
+	# We want to keep the local VO environment variables
+	export | grep VO_ | sed -e "s/^.*VO_/VO_/;s/=/ /" | while read VAR VALUE; do
+		if [ $(grep $VAR "$VO_KEEPER") ]; then
+			echo export $(grep $VAR "$VO_KEEPER")
+		else
+			echo "export $VAR=''"
+		fi
+	done > "$VO_REVERT"
+	source "$VO_REVERT"
+	rm "$VO_KEEPER" "$VO_REVERT"
 fi
