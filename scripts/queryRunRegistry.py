369b156763b1e44c631f4a9f72e605f11031718d
Fred Stober <stober@cern.ch>
2010-05-25 11:01:54 +0000

    sort lumis in queryrr
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@782 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/queryRunRegistry.py
+++ scripts/queryRunRegistry.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import gcSupport
-from grid_control.CMSSW import formatLumi, parseLumiFromJSON
+from grid_control.CMSSW import formatLumi, parseLumiFromJSON, cmpLumi
 import xmlrpclib
 
 server = xmlrpclib.ServerProxy('http://pccmsdqm04.cern.ch/runregistry/xmlrpc')
 data = server.DataExporter.export('RUNLUMISECTION', 'GLOBAL', 'json', {'groupName': 'Collisions10'})
 runs = parseLumiFromJSON(data)
+runs.sort(cmpLumi)
 
 print "lumi filter ="
 for line in map(lambda x: str.join(", ", x), gcSupport.utils.lenSplit(formatLumi(runs), 60)):
