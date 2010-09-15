e1890ac9474c57226a6997b93c90577c4b765a62
Fred Stober <stober@cern.ch>
2010-09-15 20:37:53 +0000

    New class for API misuse exceptions
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@816 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/exceptions.py
+++ python/grid_control/exceptions.py
@@ -23,10 +23,14 @@ class UserError(GridError):
 class RuntimeError(GridError):
 	pass	# just inherit everything from GridError
 
+# some error in using the API
+class APIError(GridError):
+	pass	# just inherit everything from GridError
+
 # some error with the runtime
-class AbstractError(GridError):
+class AbstractError(APIError):
 	def __init__(self):
-		GridError.__init__(self, "%s is an abstract function!" % sys._getframe(1).f_code.co_name)
+		APIError.__init__(self, "%s is an abstract function!" % sys._getframe(1).f_code.co_name)
 
 # some error with the dataset
 class DatasetError(GridError):
