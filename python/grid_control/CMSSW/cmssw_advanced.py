51ae47dd231afd8135f36c8c7d9b5c8218f6f053
Armin Burgmeier <burgmeier@ekp.uni-karlsruhe.de>
2010-09-17 11:56:48 +0000

    Fix exception when submitting grid jobs
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@828 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/CMSSW/cmssw_advanced.py
+++ python/grid_control/CMSSW/cmssw_advanced.py
@@ -91,7 +91,7 @@ class CMSSW_Advanced(cmssw.CMSSW):
 		lumifilter = self.fromNM(self.nmLumi, nick, '')
 		if lumifilter:
 			data['LUMI_RANGE'] = str.join(',', map(lambda x: '"%s"' % x, lumifilter))
-		utils.vprint('Nickname: %s' % nickname, 1)
-		utils.vprint(' * Config files: %s' % map(os.path.basename, cfg), 1)
-		utils.vprint(' *    Variables: %s' % self.nickMapConstants.get(nickname, {}), 1)
+		utils.vprint('Nickname: %s' % nick, 1)
+		utils.vprint(' * Config files: %s' % map(os.path.basename, data['CMSSW_CONFIG']), 1)
+		utils.vprint(' *    Variables: %s' % constants.get(nick, {}), 1)
 		return data
