1647cf4b1ef2d65b610af3ca517c0040763cccae
Fred Stober <stober@cern.ch>
2010-10-05 09:44:02 +0000

    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@879 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- python/grid_control/se_utils.py
+++ python/grid_control/se_utils.py
@@ -2,7 +2,7 @@ import utils
 
 # All functions use url_* functions from gc-run.lib (just like the job did...)
 
-def se_runcmd(cmd, urls):
+def se_runcmd(cmd, *urls):
 	runLib = utils.pathGC('share', 'gc-run.lib')
 	urlargs = str.join(' ', map(lambda x: '"%s"' % x.replace('dir://', 'file://'), urls))
 	return 'source %s || exit 1; print_and_eval "%s" %s' % (runLib, cmd, urlargs)
