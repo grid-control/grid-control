0194c750e52705d147f2980a90eb61d39dcd306a
Fred Stober <stober@cern.ch>
2013-07-01 10:47:13 +0000

    added automatic username extension to lfn finder
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1014 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/debugToolCMS.py
+++ scripts/debugToolCMS.py
@@ -8,11 +8,19 @@ def lfn2pfn(node, lfn):
 
 parser = optparse.OptionParser()
 parser.add_option("-s", "--SE", dest="SE", default=None, help="Resolve LFN on CMS SE into PFN")
-parser.add_option("", "--lfn", dest="lfn", default="/store/user", help="Name of default LFN")
+parser.add_option("", "--lfn", dest="lfn", default="/store/user/<hypernews name>", help="Name of default LFN")
 parser.add_option("", "--se-prot", dest="seprot", default="srmv2", help="Name of default SE protocol")
 (opts, args) = parseOptions(parser)
 
 if opts.SE:
+	if '<hypernews name>' in opts.lfn:
+		proxy = Proxy.open('VomsProxy', Config())
+		hnName = readJSON('https://cmsweb.cern.ch/sitedb/json/index/dnUserName',
+			{'dn': proxy.getFQUsername()})
+		if not hnName:
+			raise ConfigError('Unable to map grid certificate to hypernews name!')
+		opts.lfn = opts.lfn.replace('<hypernews name>', hnName['user'])
+
 	tmp = readJSON('https://cmsweb.cern.ch/phedex/datasvc/json/prod/lfn2pfn',
 		{'node': opts.SE, 'protocol': opts.seprot, 'lfn': opts.lfn})['phedex']['mapping']
 	for entry in tmp:
