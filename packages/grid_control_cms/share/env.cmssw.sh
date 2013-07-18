5a3656dcf6ed43dd811c1719970e736e4008b117
Fred Stober <stober@cern.ch>
2013-07-18 15:34:27 +0000

    removed cluster specific settings
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1030 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_cms/share/env.cmssw.sh
+++ packages/grid_control_cms/share/env.cmssw.sh
@@ -22,12 +22,6 @@ else
 		echo "[PROJ-SITE] Using $VO_CMS_SW_DIR"
 
 	# Try other software directories
-	elif [ -d "/software/kit/bd00/CMSSW" ]; then
-		export VO_CMS_SW_DIR="/software/kit/bd00/CMSSW"
-		echo "[IC1-SITE] Using $VO_CMS_SW_DIR"
-	elif [ -d "/wlcg/sw/cms" ]; then
-		export VO_CMS_SW_DIR="/wlcg/sw/cms"
-		echo "[WLCG-SITE] Using $VO_CMS_SW_DIR"
 	elif [ -d "/afs/cern.ch/cms/sw" ]; then
 		export VO_CMS_SW_DIR="/afs/cern.ch/cms/sw"
 		echo "[AFS-SITE] Using $VO_CMS_SW_DIR"
