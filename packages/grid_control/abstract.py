03cc9965340c4df33b3ea27171db20d8c4982517
Fred Stober <stober@cern.ch>
2013-11-05 18:40:37 +0000

    variable renamed
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1129 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control/abstract.py
+++ packages/grid_control/abstract.py
@@ -2,6 +2,7 @@ from python_compat import *
 from exceptions import *
 import utils
 
+# Abstract class taking care of dynamic class loading 
 class AbstractObject:
 	def __init__(self):
 		raise AbstractError
@@ -14,19 +15,19 @@ class AbstractObject:
 		cls.modPaths = utils.uniqueListRL(path + cls.modPaths + map(splitUpFun, range(cls.__module__.count(".") + 1)))
 	dynamicLoaderPath = classmethod(dynamicLoaderPath)
 
-	def getClass(cls, name):
+	def getClass(cls, clsName):
 		mjoin = lambda x: str.join('.', x)
 		# Yield search paths
 		def searchPath(cname):
 			cls.moduleMap = dict(map(lambda (k, v): (k.lower(), v), cls.moduleMap.items()))
-			name = cls.moduleMap.get(cname.lower(), cname) # resolve module mapping
-			yield name
+			fqName = cls.moduleMap.get(cname.lower(), cname) # resolve module mapping
+			yield fqName
 			for path in cls.modPaths + AbstractObject.pkgPaths:
-				if not '.' in name:
-					yield mjoin([path, name.lower(), name])
-				yield mjoin([path, name])
+				if not '.' in fqName:
+					yield mjoin([path, fqName.lower(), fqName])
+				yield mjoin([path, fqName])
 
-		for modName in searchPath(name):
+		for modName in searchPath(clsName):
 			parts = modName.split('.')
 			try: # Try to import missing modules
 				for pkg in map(lambda (i, x): mjoin(parts[:i+1]), enumerate(parts[:-1])):
@@ -39,19 +40,25 @@ class AbstractObject:
 			if issubclass(newcls, cls):
 				return newcls
 			raise ConfigError('%s is not of type %s' % (newcls, cls))
-		raise ConfigError('%s "%s" does not exist in\n\t%s!' % (cls.__name__, name, str.join('\n\t', searchPath(name))))
+		raise ConfigError('%s "%s" does not exist in\n\t%s!' % (cls.__name__, clsName, str.join('\n\t', searchPath(clsName))))
 	getClass = classmethod(getClass)
 
-	def getInstance(cls, name, *args, **kwargs):
+	def getInstance(cls, clsName, *args, **kwargs):
 		clsType = None
 		try:
-			clsType = cls.getClass(name)
+			clsType = cls.getClass(clsName)
 			return clsType(*args, **kwargs)
 		except GCError:
 			raise
 		except:
-			raise RethrowError('Error while creating instance of type %s (%s)' % (name, clsType))
+			raise RethrowError('Error while creating instance of type %s (%s)' % (clsName, clsType))
 	open = classmethod(getInstance)
 	getInstance = classmethod(getInstance)
 
 AbstractObject.pkgPaths = []
+
+
+class NamedObject(AbstractObject):
+	def __init__(self, config, name):
+		self._name = name
+		self._config = config.getScoped([self.__class__.__name__ + ' ' + name, self.__class__.__name__])
