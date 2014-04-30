421bfb25b9ba81c68225f77bc5142a9c9b68e183
Fred Stober <stober@cern.ch>
2014-04-30 08:54:18 +0000

    LICENSE update
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1261 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control/backends/glite_wms_direct.py
+++ packages/grid_control/backends/glite_wms_direct.py
@@ -1,3 +1,17 @@
+#-#  Copyright 2010-2012 Karlsruhe Institute of Technology
+#-#
+#-#  Licensed under the Apache License, Version 2.0 (the "License");
+#-#  you may not use this file except in compliance with the License.
+#-#  You may obtain a copy of the License at
+#-#
+#-#      http://www.apache.org/licenses/LICENSE-2.0
+#-#
+#-#  Unless required by applicable law or agreed to in writing, software
+#-#  distributed under the License is distributed on an "AS IS" BASIS,
+#-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#-#  See the License for the specific language governing permissions and
+#-#  limitations under the License.
+
 import os, sys
 glite = os.environ.get('GLITE_WMS_LOCATION', os.environ.get('GLITE_LOCATION', ''))
 for p in ['lib', 'lib64', os.path.join('lib', 'python'), os.path.join('lib64', 'python')]:
--- scripts/downloadFromSE.py
+++ scripts/downloadFromSE.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+#-#  Copyright 2009-2014 Karlsruhe Institute of Technology
+#-#
+#-#  Licensed under the Apache License, Version 2.0 (the "License");
+#-#  you may not use this file except in compliance with the License.
+#-#  You may obtain a copy of the License at
+#-#
+#-#      http://www.apache.org/licenses/LICENSE-2.0
+#-#
+#-#  Unless required by applicable law or agreed to in writing, software
+#-#  distributed under the License is distributed on an "AS IS" BASIS,
+#-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#-#  See the License for the specific language governing permissions and
+#-#  limitations under the License.
+
 import gcSupport, sys, os, optparse, popen2, time, random, threading
 from python_compat import md5
 from grid_control import Job, JobDB, GCError, Config, Proxy, job_selector, job_db, storage, utils, logException
