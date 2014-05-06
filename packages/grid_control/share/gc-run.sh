e73ea20259d432806a2e0dcb421435244406d1d2
Fred Stober <stober@cern.ch>
2014-05-06 11:55:49 +0000

    Updated copyright notices
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1270 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control/share/gc-run.sh
+++ packages/grid_control/share/gc-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#-#  Copyright 2010-2013 Karlsruhe Institute of Technology
+#-#  Copyright 2010-2014 Karlsruhe Institute of Technology
 #-#
 #-#  Licensed under the Apache License, Version 2.0 (the "License");
 #-#  you may not use this file except in compliance with the License.
