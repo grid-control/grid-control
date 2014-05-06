269d506e7812485c1ff48daa19fdb9fc065ef250
Fred Stober <stober@cern.ch>
2014-05-06 15:16:35 +0000

    added missing signal import
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1276 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_gui/gui_ansi.py
+++ packages/grid_control_gui/gui_ansi.py
@@ -12,7 +12,7 @@
 #-#  See the License for the specific language governing permissions and
 #-#  limitations under the License.
 
-import sys, re
+import sys, re, signal
 from ansi import Console
 from grid_control import GUI, utils, Report
 
