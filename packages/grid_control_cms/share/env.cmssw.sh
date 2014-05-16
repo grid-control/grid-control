6665a7d9df8a657229956f0fa4e103c8fc1be152
Fred Stober <stober@cern.ch>
2014-05-16 09:41:02 +0000

    Changed resolution order
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1285 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_cms/share/env.cmssw.sh
+++ packages/grid_control_cms/share/env.cmssw.sh
@@ -21,26 +21,32 @@ if [ -n "$VO_CMS_SW_DIR" ]; then
 else
 	# Fallback - try different default values for CMSSW directory
 
-	# Try CVMFS
-	if [ -d "/cvmfs/cms.cern.ch" ]; then
+	# User forced directories
+	if [ -n "$CMSSW_DIR_USER" -a -d "$CMSSW_DIR_USER" ]; then
+		export VO_CMS_SW_DIR="$CMSSW_DIR_USER"
+		echo "[USER-SITE] Using $VO_CMS_SW_DIR"
+
+	# Check CVMFS
+	elif [ -d "/cvmfs/cms.cern.ch" ]; then
 		export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 		echo "[CVMFS-SITE] Using $VO_CMS_SW_DIR"
-	# Fix for OSG sites
+
+	# OSG sites
 	elif [ -n "$OSG_APP" -a -d "$OSG_APP" ]; then
 		export VO_CMS_SW_DIR="$OSG_APP/cmssoft/cms"
 		echo "[OSG-SITE] Using $VO_CMS_SW_DIR"
 
-	# User forced directories / known during development
-	elif [ -n "$CMSSW_DIR_USER" -a -d "$CMSSW_DIR_USER" ]; then
-		export VO_CMS_SW_DIR="$CMSSW_DIR_USER"
-		echo "[USER-SITE] Using $VO_CMS_SW_DIR"
+	# Try directory used by user / known during development
 	elif [ -n "$CMSSW_DIR_PRO" -a -d "$CMSSW_DIR_PRO" ]; then
 		export VO_CMS_SW_DIR="$CMSSW_DIR_PRO"
 		echo "[PROJ-SITE] Using $VO_CMS_SW_DIR"
 
-	# Try other software directories
+	# Check AFS
 	elif [ -d "/afs/cern.ch/cms" ]; then
 		export VO_CMS_SW_DIR="/afs/cern.ch/cms"
 		echo "[AFS-SITE] Using $VO_CMS_SW_DIR"
+
+	else
+		echo "No valid location found for VO_CMS_SW_DIR!"
 	fi
 fi
