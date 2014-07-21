1353e380e64861668aa70fa1ec2a6af43449f8b1
Max Fischer <max.fischer@kit.edu>
2014-07-21 15:49:50 +0000

    fixed incorrect import
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1325 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control/processadapter.py
+++ packages/grid_control/processadapter.py
@@ -18,7 +18,7 @@ import socket
 from python_compat import *
 from exceptions import *
 from abstract import LoadableObject
-from utils import LoggedProcess, resolveInstallPath, ensureDirExists, lru_cache, QM
+from grid_control.utils import LoggedProcess, resolveInstallPath, ensureDirExists, lru_cache, QM
 
 # Container to call commands in a generic fashion
 class CommandContainer(object):
