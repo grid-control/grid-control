d9f6f476b706eced6f3fec9605f103e8ca8e51ab
Thomas Hauth <Thomas.Hauth@cern.ch>
2014-11-11 14:14:46 +0000

    added first version of the PlotReport
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1419 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/report.py
+++ scripts/report.py
@@ -13,8 +13,8 @@
 #-#  See the License for the specific language governing permissions and
 #-#  limitations under the License.
 
-import sys, optparse
-from gcSupport import utils, Config, TaskModule, JobManager, JobSelector, Report, parseOptions, handleException, getConfig
+import sys, os, optparse
+from gcSupport import utils, Config, TaskModule, JobManager, JobSelector, Report, parseOptions, handleException, getConfig, getJobInfo
 
 parser = optparse.OptionParser()
 parser.add_option('', '--report', dest='reportClass', default='GUIReport')
@@ -28,6 +28,201 @@ parser.add_option('', '--str', dest='string', default=None)
 #	help='Display time overview')
 #Report.addOptions(parser)
 (opts, args) = parseOptions(parser)
+
+
+class JobInfo ( object ) :
+	def __init__( self, payloadStart, payloadDone, seStart, seDone, outputSize, eventCount, jobFailed ): 
+		self.payloadStart = payloadStart
+		self.payloadDone = payloadDone
+		self.seStart = seStart
+		self.seDone = seDone
+		self.outputSize = outputSize
+		self.eventCount = eventCount
+		self.jobFailed = jobFailed
+
+	## todo write test code
+	def seAverageBandwithAtTimeSpan(self, timeStart, timeEnd):
+		
+		assert ( timeStart < timeEnd )
+		
+		#print " time start " + str(timeStart) + " time end " + str(timeEnd)
+		
+		## will be positive if there is overlap to the left
+		leftOutside = max(0, self.seStart - timeStart )
+		#print " -leftOutside- " + str(leftOutside)
+		## will be positive if there is overlap to the right
+		rightOutside = max(0, timeEnd - self.seDone )
+		#print " -rightOutside- " + str(rightOutside)
+		
+		totalOutside = leftOutside + rightOutside
+		fractionOutside = float( totalOutside ) / float ( timeEnd - timeStart )
+		fractionOutside = min (1.0, fractionOutside)
+		#print timeEnd - timeStart
+		#print fractionOutside
+	
+		return self.seBandwidth() * ( 1.0 - fractionOutside )
+
+	def seStageOutActive(self, timeStart, timeEnd):
+		assert ( timeStart < timeEnd )
+		
+		#print " time start " + str(timeStart) + " time end " + str(timeEnd)
+		
+		## will be positive if there is overlap to the left
+		leftOutside = max(0, self.seStart - timeStart )
+		#print " -leftOutside- " + str(leftOutside)
+		## will be positive if there is overlap to the right
+		rightOutside = max(0, timeEnd - self.seDone )
+		#print " -rightOutside- " + str(rightOutside)
+		
+		totalOutside = leftOutside + rightOutside
+		fractionOutside = float( totalOutside ) / float ( timeEnd - timeStart )
+		fractionOutside = min (1.0, fractionOutside)
+		#print timeEnd - timeStart
+		#print fractionOutside
+	
+		return 1.0 * ( 1.0 - fractionOutside )
+
+	# in MB / second
+	def seBandwidth(self):
+		return ( self.outputSize / 1000000.0 ) / self.seRuntime()
+
+	def payloadRuntime(self):
+		return self.payloadDone - self.payloadStart
+
+	def seRuntime(self):
+		return self.seDone - self.seStart
+
+
+#def makeEnumAndMapping( entryNames )
+#    enums = utils.makeEnum( entryNames )
+#    mappingStringToEnum = dict()
+	
+#    for i in range(0, len ( entryNames ) entryName
+
+
+
+TIMESTAMP_WRAPPER_START=1415358689
+TIMESTAMP_DEPLOYMENT_START=1415358689
+TIMESTAMP_DEPLOYMENT_DONE=1415358689
+TIMESTAMP_SE_IN_START=1415358689
+TIMESTAMP_SE_IN_DONE=1415358689
+TIMESTAMP_EXECUTION_START=1415358689
+TIMESTAMP_EXECUTION_DONE=1415358694
+TIMESTAMP_SE_OUT_START=1415358694
+TIMESTAMP_SE_OUT_DONE=1415358694
+TIMESTAMP_WRAPPER_DONE=1415358694
+
+JobResultEnum = utils.makeEnum( [ "TIMESTAMP_WRAPPER_START", 
+"TIMESTAMP_DEPLOYMENT_START", 
+"TIMESTAMP_DEPLOYMENT_DONE",
+"TIMESTAMP_SE_IN_START",
+"TIMESTAMP_SE_IN_DONE", 
+"TIMESTAMP_CMSSW_CMSRUN1_START",
+"TIMESTAMP_CMSSW_CMSRUN1_DONE",
+"TIMESTAMP_EXECUTION_START",
+"TIMESTAMP_EXECUTION_DONE",
+"TIMESTAMP_SE_OUT_START",
+"TIMESTAMP_SE_OUT_DONE",
+"TIMESTAMP_WRAPPER_DONE" ] )
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def extractJobTiming( jInfo ):
+	jobResult = dict()
+	
+	#intialize all with None
+	for key in JobResultEnum.members:
+		enumID = JobResultEnum.fromString( key )
+		jobResult[ enumID ] = None
+	
+	for ( key, val ) in jInfo.iteritems():
+		enumID = JobResultEnum.fromString( key )
+		if ( enumID != None ):
+			jobResult[ enumID ] = val
+
+	return jobResult
+	#print JobResultEnum.members
+	#pi = JobResultEnum.members.index("payloadDone")
+	#myEnum = JobResultEnum
+	#myEnum.setFromKey( "outputSize" )
+	#print myEnum.fromString( "TIMESTAMP_WRAPPER_DONE" )
+	#print timingDict
+
+
+# returns the job payload runtime in seconds
+# - if a CMSSW job was run, only the time spend in the actual
+#   cmsRun call will be reported
+# - if a user job was run, the execution time of the user job 
+#   will be reported
+def getPayloadRuntime ( jobInfo ):
+#	print jobInfo
+#	print JobResultEnum.TIMESTAMP_EXECUTION_DONE
+	if ( jobInfo [ JobResultEnum.TIMESTAMP_CMSSW_CMSRUN1_START ] != None ) and \
+		( jobInfo [ JobResultEnum.TIMESTAMP_CMSSW_CMSRUN1_DONE ] != None ):
+		return jobInfo [ JobResultEnum.TIMESTAMP_CMSSW_CMSRUN1_DONE ] - \
+				jobInfo [ JobResultEnum.TIMESTAMP_CMSSW_CMSRUN1_START ]
+				
+	return jobInfo [ JobResultEnum.TIMESTAMP_EXECUTION_DONE ] - \
+				jobInfo [ JobResultEnum.TIMESTAMP_EXECUTION_START ]
+
+class PlotReport( Report ):
+	
+	
+	def initHistogram ( self, name, xlabel, ylabel ):
+		fig = plt.figure()
+
+		ax = fig.add_subplot(111)
+		ax.set_xlabel(r"Time (s)", ha="right", x=1)
+		ax.set_ylabel(r"Bandwith (MB/s)", va="top", y=1, labelpad = 20.0)
+		
+		return (name, fig, ax)
+	
+	def finalizeHistogram( self, plotSet, useLegend = False ):
+		if ( useLegend ):
+			plt.legend(loc="upper right", numpoints=1, frameon=False, ncol=2)
+		
+		self.imageTypes = ["png", "pdf"]
+		for it in self.imageTypes:
+			plt.savefig( plotSet[0] + "." + it ) 
+
+	def plotPayloadRuntime ( self, histo, jobResult ):
+
+		runtime = []
+		for res in jobResult:
+			runtime = runtime + [ getPayloadRuntime( res ) ]
+
+		thisHistType = "bar"
+		#if transparent:
+		#	thisHistType = "step"
+
+		pl = plt.hist( runtime, 40 )#, color= plotColor, label = plotLabel, histtype=thisHistType )
+		return pl
+		
+
+	def display(self):
+		jobResult = []
+		
+		allJobs = self._jobDB.getJobs()
+		workdir = os.path.join( self._jobDB._dbPath, ".." ) 
+		for j in allJobs:
+			job = self._jobDB.get( j )
+			#print job.state
+			
+			jInfo = getJobInfo ( workdir, j, preserveCase = True )
+			#print jInfo
+			if ( jInfo == None ):
+				continue
+				
+			jResult = extractJobTiming( jInfo )
+			jobResult = jobResult + [jResult]
+			#print jobResult
+			
+		
+		histo =	self.initHistogram( "payload_runtime", "Payload Runtime (s)", "Count" )
+		self.plotPayloadRuntime( histo, jobResult )
+		self.finalizeHistogram( histo )
+
 
 if len(args) != 1:
 	utils.exitWithUsage('%s [options] <config file>' % sys.argv[0])
