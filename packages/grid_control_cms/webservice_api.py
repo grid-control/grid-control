1cd5cd8bebac9adb8ff3d7c9d26173e8d9037532
Manuel Giffels <Manuel.Giffels@cern.ch>
2015-02-06 10:20:00 +0000

    Adds sendJSON functionalty. Solves a problem with HTTPS requests to DBS3 Raphael noticed (solved by removing install_opener).
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1488 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_cms/webservice_api.py
+++ packages/grid_control_cms/webservice_api.py
@@ -14,44 +14,95 @@
 
 # Helper to access CMS webservices (SiteDB, Phedex)
 
+def setupHTTPSClientAuthHandler(key, cert):
+    import urllib2, httplib
+
+    class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
+        def __init__(self, key, cert):
+            urllib2.HTTPSHandler.__init__(self)
+            self.key, self.cert = key, cert
+        def https_open(self, req):
+            return self.do_open(self.getConnection, req)
+        def getConnection(self, host, timeout = None):
+            return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
+    return HTTPSClientAuthHandler(key, cert)
+
 def removeUnicode(obj):
-	if type(obj) in (list, tuple, set):
-		(obj, oldType) = (list(obj), type(obj))
-		for i, v in enumerate(obj):
-			obj[i] = removeUnicode(v)
-		obj = oldType(obj)
-	elif isinstance(obj, dict):
-		result = {}
-		for k, v in obj.iteritems():
-			result[removeUnicode(k)] = removeUnicode(v)
-		return result
-	elif isinstance(obj, unicode):
-		return str(obj)
-	return obj
+    if type(obj) in (list, tuple, set):
+        (obj, oldType) = (list(obj), type(obj))
+        for i, v in enumerate(obj):
+            obj[i] = removeUnicode(v)
+        obj = oldType(obj)
+    elif isinstance(obj, dict):
+        result = {}
+        for k, v in obj.iteritems():
+            result[removeUnicode(k)] = removeUnicode(v)
+        return result
+    elif isinstance(obj, unicode):
+        return str(obj)
+    return obj
 
 def readURL(url, params = None, headers = {}, cert = None):
-	import urllib, urllib2, httplib
-	class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
-		def __init__(self, key, cert):
-			urllib2.HTTPSHandler.__init__(self)
-			(self.key, self.cert) = (key, cert)
-		def https_open(self, req):
-			return self.do_open(self.getConnection, req)
-		def getConnection(self, host, timeout = None):
-			return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
+    import urllib, urllib2
 
-	if cert:
-		cert_handler = HTTPSClientAuthHandler(cert, cert)
-		opener = urllib2.build_opener(cert_handler)
-		urllib2.install_opener(opener)
+    if cert:
+        cert_handler = setupHTTPSClientAuthHandler(cert, cert)
+        opener = urllib2.build_opener(cert_handler)
+    else:
+        opener = urllib2.build_opener()
 
-	if params:
-		url += '?%s' % urllib.urlencode(params)
-	return urllib2.urlopen(urllib2.Request(url, None, headers)).read()
+    if params:
+        url += '?%s' % urllib.urlencode(params)
+    return opener.open(urllib2.Request(url, None, headers)).read()
 
 def parseJSON(data):
-	import json
-	return removeUnicode(json.loads(data.replace('\'', '"')))
+    import json
+    return removeUnicode(json.loads(data.replace('\'', '"')))
 
 def readJSON(url, params = None, headers = {}, cert = None):
-	return parseJSON(readURL(url, params, headers, cert))
+    return parseJSON(readURL(url, params, headers, cert))
+
+def sendJSON(url, data=None, params=None, headers=None, cert=None, method='POST'):
+    import json, urllib, urllib2
+
+    if cert:
+        cert_handler = setupHTTPSClientAuthHandler(cert, cert)
+        opener = urllib2.build_opener(cert_handler)
+    else:
+        opener = urllib2.build_opener()
+
+    if params:
+        url += '?%s' % urllib.urlencode(params)
+
+    if headers:
+        ###do not change originating dictionary
+        headers = dict(headers)
+        headers.update({"Content-type": 'application/json',
+                        "Accept": 'application/json'})
+    else:
+        headers = {"Content-type": 'application/json',
+                   "Accept": 'application/json'}
+
+    data = json.dumps(data)
+    #necessary to optimize performance of CherryPy
+    headers['Content-length'] = str(len(data))
+
+    request = urllib2.Request(url=url, data=data, headers=headers)
+    request.get_method = lambda: method
+
+    try:
+        http_data = opener.open(request)
+    except urllib2.HTTPError as http_error:
+        #DBS 3 is returning an error in json format, PhEDEx is returning a string.
+        # Print the error and re-raise the exception
+        data = http_error.read()
+        try:
+            data = parseJSON(data)
+        except ValueError:
+            pass
+        finally:
+            print data
+            raise http_error
+    else:
+        return parseJSON(http_data.read())
+    
