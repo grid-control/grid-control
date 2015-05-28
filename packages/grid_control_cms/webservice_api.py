959064f34212a6d78b43f13f5a2471cb69f56979
Manuel Giffels <Manuel.Giffels@cern.ch>
2015-05-28 11:51:13 +0000

    Added REST client to webservice_api. Use requests library because of ssl session caching, if available. Fallback urllib2.
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1503 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_cms/webservice_api.py
+++ packages/grid_control_cms/webservice_api.py
@@ -12,10 +12,16 @@
 #-#  See the License for the specific language governing permissions and
 #-#  limitations under the License.
 
-# Helper to access CMS webservices (SiteDB, Phedex)
+# Helper to access CMS web-services (DBS, SiteDB, PhEDEx)
 
-def setupHTTPSClientAuthHandler(key, cert):
-    import urllib2, httplib
+import urllib
+import urllib2
+import httplib
+
+try:
+    import requests
+except ImportError:
+    #fall back to urllib2
 
     #fix ca verification error in Python 2.7.9
     try:
@@ -25,14 +31,165 @@ def setupHTTPSClientAuthHandler(key, cert):
         pass
 
     class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
-        def __init__(self, key, cert):
+        def __init__(self, key=None, cert=None):
             urllib2.HTTPSHandler.__init__(self)
             self.key, self.cert = key, cert
         def https_open(self, req):
             return self.do_open(self.getConnection, req)
         def getConnection(self, host, timeout = None):
             return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
-    return HTTPSClientAuthHandler(key, cert)
+
+    class RestClient(object):
+        def __init__(self, cert=None, default_headers=None):
+            self.cert = cert
+            self.headers = default_headers or {"Content-type": 'application/json',
+                                               "Accept": 'application/json'}
+
+        def _build_opener(self):
+            if self.cert:
+                cert_handler = HTTPSClientAuthHandler(self.cert, self.cert)
+                return urllib2.build_opener(cert_handler)
+            else:
+                return urllib2.build_opener()
+
+        def _format_url(self, url, api, params):
+            if api:
+                url += '/%s' % api
+
+            if params:
+                url += '?%s' % urllib.urlencode(params)
+
+            return url
+
+        def _request_headers(self, headers):
+            request_headers = {}
+            request_headers.update(self.headers)
+
+            if headers:
+                request_headers.update(headers)
+
+            return request_headers
+
+        def get(self, url, api=None, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+
+            url = self._format_url(url, api, params)
+            opener = self._build_opener()
+            return opener.open(urllib2.Request(url, None, request_headers)).read()
+
+        def post(self, url, api, data=None, headers=None):
+            request_headers = self._request_headers(headers)
+            #necessary to optimize performance of CherryPy
+            request_headers['Content-length'] = str(len(data))
+
+            request = urllib2.Request(url='%s/%s' % (url, api) if api else url, data=data, headers=headers)
+            request.get_method = lambda: "POST"
+
+            opener = self._build_opener()
+            return opener.open(request).read()
+
+        def put(self, url, api, data=None, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+            #necessary to optimize performance of CherryPy
+            request_headers['Content-length'] = str(len(data))
+
+            url = self._format_url(url, api, params)
+            request = urllib2.Request(url=url, data=data, headers=headers)
+            request.get_method = lambda: "PUT"
+
+            opener = self._build_opener()
+            return opener.open(request).read()
+
+        def delete(self, url, api, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+
+            url = self._format_url(url, api, params)
+            request = urllib2.Request(url=url, headers=request_headers)
+            request.get_method = lambda: "DELETE"
+
+            opener = self._build_opener()
+            return opener.open(request).read()
+
+else:
+    from requests.exceptions import HTTPError
+    #disable ssl ca verification errors
+    requests.packages.urllib3.disable_warnings()
+
+    class RestClient(object):
+        _requests_client_session = None
+
+        def __init__(self, cert=None, default_headers=None):
+            if not self._requests_client_session:
+                self._requests_client_session = requests.Session()
+            self.cert = cert
+            self.headers = default_headers or {"Content-type": 'application/json',
+                                               "Accept": 'application/json'}
+
+        def _raise_for_status(self, response):
+            """
+            checks for status not ok and raises corresponding http error
+            """
+            try:
+                response.raise_for_status()
+            except HTTPError as http_error:
+                msg = {'server_response': http_error.message,
+                       'additional_information': response.text}
+                raise HTTPError("%(server_response)s\n%(additional_information)s" % msg)
+
+        def _request_headers(self, headers):
+            request_headers = {}
+            request_headers.update(self.headers)
+
+            if headers:
+                request_headers.update(headers)
+
+            return request_headers
+
+        def get(self, url, api=None, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+
+            response = self._requests_client_session.get(url='%s/%s' % (url, api) if api else url,
+                                                         verify=False,
+                                                         cert=self.cert,
+                                                         headers=request_headers,
+                                                         params=params)
+            self._raise_for_status(response)
+            return response.text
+
+        def post(self, url, api, data=None, headers=None):
+            request_headers = self._request_headers(headers)
+
+            response = self._requests_client_session.post(url='%s/%s' % (url, api) if api else url,
+                                                          verify=False,
+                                                          cert=self.cert,
+                                                          headers=request_headers,
+                                                          data=data)
+            self._raise_for_status(response)
+            return response.text
+
+        def put(self, url, api, data=None, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+
+            response = self._requests_client_session.put(url='%s/%s' % (url, api) if api else url,
+                                                         verify=False,
+                                                         cert=self.cert,
+                                                         headers=request_headers,
+                                                         data=data,
+                                                         params=params)
+            self._raise_for_status(response)
+            return response.text
+
+        def delete(self, url, api, headers=None, params=None):
+            request_headers = self._request_headers(headers)
+
+            response = self._requests_client_session.delete(url='%s/%s' % (url, api) if api else url,
+                                                            verify=False,
+                                                            cert=self.cert,
+                                                            headers=request_headers,
+                                                            params=params)
+            self._raise_for_status(response)
+            return response.text
+
 
 def removeUnicode(obj):
     if type(obj) in (list, tuple, set):
@@ -49,66 +206,13 @@ def removeUnicode(obj):
         return str(obj)
     return obj
 
-def readURL(url, params = None, headers = {}, cert = None):
-    import urllib, urllib2
-
-    if cert:
-        cert_handler = setupHTTPSClientAuthHandler(cert, cert)
-        opener = urllib2.build_opener(cert_handler)
-    else:
-        opener = urllib2.build_opener()
-
-    if params:
-        url += '?%s' % urllib.urlencode(params)
-    return opener.open(urllib2.Request(url, None, headers)).read()
+def readURL(url, params=None, headers=None, cert=None):
+    rest_client = RestClient(cert)
+    return rest_client.get(url=url, headers=headers, params=params)
 
 def parseJSON(data):
     import json
     return removeUnicode(json.loads(data.replace('\'', '"')))
 
-def readJSON(url, params = None, headers = {}, cert = None):
+def readJSON(url, params = None, headers = None, cert = None):
     return parseJSON(readURL(url, params, headers, cert))
-
-def sendJSON(url, data=None, params=None, headers=None, cert=None, method='POST'):
-    import json, urllib, urllib2
-
-    if cert:
-        cert_handler = setupHTTPSClientAuthHandler(cert, cert)
-        opener = urllib2.build_opener(cert_handler)
-    else:
-        opener = urllib2.build_opener()
-
-    if params:
-        url += '?%s' % urllib.urlencode(params)
-
-    if headers:
-        ###do not change originating dictionary
-        headers = dict(headers)
-        headers.update({"Content-type": 'application/json',
-                        "Accept": 'application/json'})
-    else:
-        headers = {"Content-type": 'application/json',
-                   "Accept": 'application/json'}
-
-    data = json.dumps(data)
-    #necessary to optimize performance of CherryPy
-    headers['Content-length'] = str(len(data))
-
-    request = urllib2.Request(url=url, data=data, headers=headers)
-    request.get_method = lambda: method
-
-    try:
-        http_data = opener.open(request)
-    except urllib2.HTTPError as http_error:
-        #DBS 3 is returning an error in json format, PhEDEx is returning a string.
-        # Print the error and re-raise the exception
-        data = http_error.read()
-        try:
-            data = parseJSON(data)
-        except ValueError:
-            pass
-        finally:
-            print data
-            raise http_error
-    else:
-        return parseJSON(http_data.read())
