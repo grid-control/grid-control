32548dcd8eada9b4f99e13891d8dc2abc85ca95f
Fred Stober <stober@cern.ch>
2016-03-18 00:07:48 +0000

    Refactored LocalProcess to unify stdout/stderr handling and fix python3 issues Reduced dependencies of LocalProcess
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1665 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control/utils/process_base.py
+++ packages/grid_control/utils/process_base.py
@@ -15,15 +15,12 @@
 import os, pty, sys, time, errno, fcntl, select, signal, logging, termios, threading
 from grid_control.utils.thread_tools import GCEvent, GCLock, GCQueue
 from hpfwk import AbstractError
-from python_compat import ifilter, imap, irange, lmap
+from python_compat import bytes2str, irange, str2bytes
 
 try:
 	FD_MAX = os.sysconf('SC_OPEN_MAX')
 except (AttributeError, ValueError):
 	FD_MAX = 256
-
-SIGNAL_DICT = dict(imap(lambda s: (getattr(signal, s), s),\
-	ifilter(lambda c: c.startswith('SIG') and ('_' not in c), dir(signal))))
 
 def safeClose(fd):
 	try:
@@ -31,21 +28,122 @@ def safeClose(fd):
 	except OSError:
 		pass
 
-class ProcessTimeout(Exception):
+class ProcessError(Exception):
 	pass
+
+class ProcessTimeout(ProcessError):
+	pass
+
+
+class ProcessStream(object):
+	def __init__(self, buffer, log):
+		(self._buffer, self._log) = (buffer, log)
+
+	def read_log(self):
+		if self._log is not None:
+			return self._log
+		return ''
+
+	def clear_log(self):
+		result = self._log
+		if self._log is not None:
+			self._log = ''
+		return result
+
+	def __repr__(self):
+		return '%s(buffer = %r)' % (self.__class__.__name__, self.read_log())
+
+
+class ProcessReadStream(ProcessStream):
+	def __init__(self, buffer, event_shutdown, event_finished, log = None):
+		ProcessStream.__init__(self, buffer, log)
+		(self._event_shutdown, self._event_finished, self._iter_buffer) = (event_shutdown, event_finished, '')
+
+	def read(self, timeout):
+		result = self._buffer.get(timeout)
+		if self._log is not None:
+			self._log += result
+		return result
+
+	def read_log(self):
+		result = self.read(0) # flush buffer
+		return ProcessStream.read_log(self) or result
+
+	# wait until stream fulfills condition
+	def wait(self, timeout, cond):
+		result = ''
+		status = None
+		timeout_left = timeout
+		while True:
+			t_start = time.time()
+			result += self.read(timeout = timeout_left)
+			timeout_left -= time.time() - t_start
+			if cond(result):
+				break
+			if status is not None: # check before update to make at least one last read from stream
+				break
+			status = self.status(0)
+			if timeout_left < 0:
+				raise ProcessTimeout('Stream result did not fulfill condition after waiting for %d seconds' % timeout)
+		return result
+
+	def iter(self, timeout, timeout_soft = False, timeout_shutdown = 10):
+		waitedForShutdown = False
+		while True:
+			# yield lines from buffer
+			while self._iter_buffer.find('\n') != -1:
+				posEOL = self._iter_buffer.find('\n')
+				yield self._iter_buffer[:posEOL + 1]
+				self._iter_buffer = self._iter_buffer[posEOL + 1:]
+			# block until new data in buffer / timeout or process is finished
+			tmp = self._buffer.get(timeout)
+			if tmp: # new data
+				self._iter_buffer += tmp
+			elif self._event_shutdown.is_set() and not waitedForShutdown: # shutdown in progress
+				waitedForShutdown = True
+				self._event_finished.wait(timeout_shutdown, 'process shutdown to complete') # wait for shutdown to complete
+			elif self._event_finished.is_set() or timeout_soft:
+				break # process finished / soft timeout
+			else:
+				raise ProcessTimeout('Stream did not yield more lines after waiting for %d seconds' % timeout) # hard timeout
+		if self._iter_buffer: # return rest of buffer
+			yield self._iter_buffer
+
+
+class ProcessWriteStream(ProcessStream):
+	def __init__(self, buffer, close_token = None, log = None):
+		ProcessStream.__init__(self, buffer, log)
+		self._close_token = close_token
+
+	def write(self, value, log = True):
+		if log and (self._log is not None):
+			self._log += value
+		self._buffer.put(value)
+
+	def set_eof(self, value):
+		self._close_token = value
+
+	def close(self):
+		self.write(self._close_token)
+
 
 class Process(object):
 	def __init__(self, cmd, *args, **kwargs):
-		self._process_finished = GCEvent()
+		self._event_shutdown = GCEvent()
+		self._event_finished = GCEvent()
 		self._buffer_stdin = GCQueue(str)
 		self._buffer_stdout = GCQueue(str)
 		self._buffer_stderr = GCQueue(str)
-		self._iter_buffer_stdout = ''
-		# Stream logging setup
-		self._logging = kwargs.get('logging', True)
-		self.clear_logs()
+		# Stream setup
+		do_log = kwargs.get('logging', True) or None
+		self.stdout = ProcessReadStream(self._buffer_stdout, self._event_shutdown, self._event_finished, log = do_log)
+		self.stderr = ProcessReadStream(self._buffer_stderr, self._event_shutdown, self._event_finished, log = do_log)
+		self.stdin = ProcessWriteStream(self._buffer_stdin, log = do_log)
+		self.clear_logs() # reset log to proper start value
 
-		args = lmap(str, args)
+		self._args = []
+		for arg in args:
+			self._args.append(str(arg))
 		if not cmd:
 			raise RuntimeError('Invalid executable!')
 		if not os.path.isabs(cmd): # Resolve executable path
@@ -56,21 +154,26 @@ class Process(object):
 		if not os.access(cmd, os.X_OK):
 			raise OSError('Unable to execute %r' % cmd)
 		self._log = logging.getLogger('process.%s' % os.path.basename(cmd))
-		self._log.debug('External programm called: %s %s', cmd, args)
-		(self._cmd, self._args) = (cmd, lmap(str, args))
+		self._log.debug('External programm called: %s %s', cmd, self._args)
+		self._cmd = cmd
 		self.start()
 
-	def clear_logs(self):
-		(self._log_stdin, self._log_stdout, self._log_stderr) = ('', '', '')
-
 	def __repr__(self):
-		return '%s(status = %s, flushed stdout = %r, flushed stderr = %r)' % (
-			self.__class__.__name__, self.status(0), self.read_stdout_log(), self.read_stderr_log())
+		return '%s(cmd = %s, args = %s, status = %s, flushed stdout = %r, flushed stderr = %r)' % (
+			self.__class__.__name__, self._cmd, repr(self._args), self.status(0), self.stdout.read_log(), self.stderr.read_log())
+
+	def clear_logs(self):
+		self.stdout.clear_log()
+		self.stderr.clear_log()
+		self.stdin.clear_log()
 
 	def get_call(self):
-		return '%s %s' % (self._cmd, str.join(' ', self._args))
+		return str.join(' ', [self._cmd] + self._args)
 
 	def start(self):
+		raise AbstractError
+
+	def terminate(self, timeout):
 		raise AbstractError
 
 	def kill(self, sig = signal.SIGTERM):
@@ -84,93 +187,39 @@ class Process(object):
 	def status(self, timeout, terminate = False):
 		raise AbstractError
 
+	def status_raise(self, timeout):
+		status = self.status(timeout)
+		if status is None:
+			self.terminate(timeout = 1)
+			raise ProcessTimeout('Process is still running after waiting for %d seconds' % timeout) # hard timeout
+		return status
+
+	def get_output(self, timeout, raise_errors = False):
+		t_end = time.time() + timeout
+		result = self.stdout.read(timeout)
+		status = self.status(timeout = max(0, t_end - time.time()))
+		if status is None:
+			self.terminate(timeout = 1)
+		if raise_errors and (status is None):
+			raise ProcessTimeout('Process is still running after waiting for %d seconds' % timeout)
+		elif raise_errors and (status != 0):
+			raise ProcessError('Command %s %s returned with exit code %s' % (self._cmd, repr(self._args), status))
+		return result
+
 	def finish(self, timeout):
-		result = self.status(timeout)
-		tmp = (result, self.read_stdout(timeout = 0), self.read_stderr(timeout = 0))
-		if result is None:
-			raise ProcessTimeout # hard timeout
-		return tmp
-
-	def _wait_stream(self, read_stream_fun, timeout, cond):
-		result = ''
-		state = None
-		while True:
-			t_start = time.time()
-			result += read_stream_fun(timeout = timeout)
-			timeout -= time.time() - t_start
-			if cond(result):
-				break
-			if state is not None: # check before update to make at least one last read from stream
-				break
-			state = self.status(0)
-			if timeout < 0:
-				raise ProcessTimeout
-		return result
-
-	def read_stdout(self, timeout):
-		result = self._buffer_stdout.get(timeout)
-		if self._logging:
-			self._log_stdout += result
-		return result
-
-	def read_stdout_log(self):
-		self.read_stdout(0) # flush buffer into log
-		return self._log_stdout
-
-	def wait_stdout(self, timeout, cond):
-		return self._wait_stream(self.read_stdout, timeout, cond)
-
-	def read_stderr(self, timeout):
-		result = self._buffer_stderr.get(timeout)
-		if self._logging:
-			self._log_stderr += result
-		return result
-
-	def read_stderr_log(self):
-		self.read_stderr(0) # flush buffer into log
-		return self._log_stderr
-
-	def wait_stderr(self, timeout, cond):
-		return self._wait_stream(self.read_stderr, timeout, cond)
-
-	def write_stdin(self, value, log = True):
-		if log and self._logging:
-			self._log_stdin += value
-		self._buffer_stdin.put(value)
-
-	def read_stdin_log(self):
-		return self._log_stdin
-
-	def close_stdin(self):
-		raise AbstractError
-
-	def iter_stdout(self, timeout, timeout_soft = False, timeout_shutdown = 10):
-		waitedForShutdown = False
-		while True:
-			# yield lines from buffer
-			while self._iter_buffer_stdout.find('\n') != -1:
-				posEOL = self._iter_buffer_stdout.find('\n')
-				yield self._iter_buffer_stdout[:posEOL + 1]
-				self._iter_buffer_stdout = self._iter_buffer_stdout[posEOL + 1:]
-			# block until new data in buffer / timeout or process is finished
-			tmp = self._buffer_stdout.get(timeout)
-			if tmp: # new data
-				self._iter_buffer_stdout += tmp
-			elif self._process_shutdown.is_set() and not waitedForShutdown: # shutdown in progress
-				waitedForShutdown = True
-				self._process_finished.wait(timeout_shutdown, 'process shutdown to complete') # wait for shutdown to complete
-			elif self._process_finished.is_set() or timeout_soft:
-				break # process finished / soft timeout
-			else:
-				raise ProcessTimeout # hard timeout
-		if self._iter_buffer_stdout: # return rest of buffer
-			yield self._iter_buffer_stdout
+		status = self.status_raise(timeout)
+		return (status, self.stdout.read(timeout = 0), self.stderr.read(timeout = 0))
 
 
 class LocalProcess(Process):
 	def __init__(self, cmd, *args, **kwargs):
 		self._status = None
 		Process.__init__(self, cmd, *args, **kwargs)
+		self.stdin.set_eof(chr(ord(termios.tcgetattr(self._fd_terminal)[6][termios.VEOF])))
+		self._signal_dict = {}
+		for attr in dir(signal):
+			if attr.startswith('SIG') and ('_' not in attr):
+				self._signal_dict[getattr(signal, attr)] = attr
 
 	def start(self):
 		# Setup of file descriptors
@@ -191,7 +240,8 @@ class LocalProcess(Process):
 			try:
 				os.execv(self._cmd, [self._cmd] + self._args)
 			except Exception:
-				sys.stderr.write('Error while calling os.execvp: ' + repr(sys.exc_info()[1]))
+				invoked = 'os.execv(%s, [%s] + %s)' % (repr(self._cmd), repr(self._cmd), repr(self._args))
+				sys.stderr.write('Error while calling %s: ' % invoked + repr(sys.exc_info()[1]))
 				for fd in irange(0, 3):
 					safeClose(fd)
 				os._exit(os.EX_OSERR)
@@ -201,20 +251,19 @@ class LocalProcess(Process):
 			safeClose(fd_child_stderr)
 			for fd in [fd_parent_stdout, fd_parent_stderr]:
 				fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK | fcntl.fcntl(fd, fcntl.F_GETFL))
-			self._process_shutdown = GCEvent() # flag to start shutdown of output handlers
-			self._process_shutdown_stdin = GCEvent() # flag to start shutdown of input handlers
+			self._event_shutdown_stdin = GCEvent() # flag to start shutdown of input handlers
 
 			def handleOutput(fd, buffer):
 				def readToBuffer():
 					while True:
 						try:
-							tmp = os.read(fd, 32*1024)
+							tmp = bytes2str(os.read(fd, 32*1024))
 						except OSError:
 							tmp = ''
 						if not tmp:
 							break
 						buffer.put(tmp)
-				while not self._process_shutdown.is_set():
+				while not self._event_shutdown.is_set():
 					try:
 						select.select([fd], [], [], 0.2)
 					except Exception:
@@ -225,7 +274,7 @@ class LocalProcess(Process):
 
 			def handleInput():
 				local_buffer = ''
-				while not self._process_shutdown.is_set():
+				while not self._event_shutdown.is_set():
 					if local_buffer: # if local buffer ist leftover from last write - just poll for more
 						local_buffer += self._buffer_stdin.get(timeout = 0)
 					else: # empty local buffer - wait for data to process
@@ -235,10 +284,10 @@ class LocalProcess(Process):
 							(rl, write_list, xl) = select.select([], [fd_parent_stdin], [], 0.2)
 						except Exception:
 							pass
-						if write_list and not self._process_shutdown.is_set():
-							written = os.write(fd_parent_stdin, local_buffer)
+						if write_list and not self._event_shutdown.is_set():
+							written = os.write(fd_parent_stdin, str2bytes(local_buffer))
 							local_buffer = local_buffer[written:]
-					elif self._process_shutdown_stdin.is_set():
+					elif self._event_shutdown_stdin.is_set():
 						break
 				safeClose(fd_parent_stdin)
 
@@ -256,15 +305,15 @@ class LocalProcess(Process):
 						(pid, status) = (self._pid, -1)
 					if pid == self._pid:
 						self._status = status
-				self._process_shutdown.set() # start shutdown of handlers and wait for it to finish
-				self._process_shutdown_stdin.set() # start shutdown of handlers and wait for it to finish
+				self._event_shutdown.set() # start shutdown of handlers and wait for it to finish
+				self._event_shutdown_stdin.set() # start shutdown of handlers and wait for it to finish
 				self._buffer_stdin.finish() # wakeup process input handler
 				thread_in.join()
 				thread_out.join()
 				thread_err.join()
 				self._buffer_stdout.finish() # wakeup pending output buffer waits
 				self._buffer_stderr.finish()
-				self._process_finished.set()
+				self._event_finished.set()
 
 			thread = threading.Thread(target = checkStatus)
 			thread.daemon = True
@@ -281,22 +330,28 @@ class LocalProcess(Process):
 		self.write_stdin(chr(ord(termios.tcgetattr(self._fd_terminal)[6][termios.VEOF])))
 
 	def status(self, timeout, terminate = False):
-		self._process_finished.wait(timeout, 'process to finish')
+		self._event_finished.wait(timeout, 'process to finish')
 		if self._status is not None: # return either signal name or exit code
 			if os.WIFSIGNALED(self._status):
-				return SIGNAL_DICT.get(os.WTERMSIG(self._status), 'SIG_UNKNOWN')
+				return self._signal_dict.get(os.WTERMSIG(self._status), 'SIG_UNKNOWN')
 			elif os.WIFEXITED(self._status):
 				return os.WEXITSTATUS(self._status)
 		if terminate:
-			self.kill(signal.SIGTERM)
-			result = self.status(timeout = 1, terminate = False)
-			if result is not None:
-				return result
-			self.kill(signal.SIGKILL)
-			return self.status(timeout = 1, terminate = False)
+			return self.terminate(timeout = 1)
+
+	def terminate(self, timeout):
+		status = self.status(timeout = 0)
+		if status is not None:
+			return status
+		self.kill(signal.SIGTERM)
+		result = self.status(timeout, terminate = False)
+		if result is not None:
+			return result
+		self.kill(signal.SIGKILL)
+		return self.status(timeout, terminate = False)
 
 	def kill(self, sig = signal.SIGTERM):
-		if not self._process_finished.is_set():
+		if not self._event_finished.is_set():
 			try:
 				os.kill(self._pid, sig)
 			except OSError:
