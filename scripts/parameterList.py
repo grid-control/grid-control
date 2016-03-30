88971f73c1b459550a41d09f375489b70da9de76
Fred Stober <stober@cern.ch>
2016-03-30 21:30:34 +0000

    Refactored parameterList.py
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1685 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/parameterList.py
+++ scripts/parameterList.py
@@ -43,7 +43,59 @@ parser.addText(None, 'visible',            default = '',    short = '-V', help =
 if len(args) != 1:
 	utils.exitWithUsage(usage)
 
-def main():
+# Create dataset parameter source
+class DummySplitter:
+	def getMaxJobs(self):
+		return 3
+	def getSplitInfo(self, pNum):
+		mkEntry = lambda ds, fl, n, nick: { DataSplitter.Dataset: ds, DataSplitter.Nickname: nick,
+			DataSplitter.FileList: fl, DataSplitter.NEntries: n }
+		tmp = [ mkEntry('ds1', ['a', 'b'], 23, 'data_1'), mkEntry('ds1', ['1'], 42, 'data_1'),
+			mkEntry('ds2', ['m', 'n'], 123, 'data_2'), mkEntry('ds2', ['x', 'y', 'z'], 987, 'data_3') ]
+		return tmp[pNum]
+
+class DataSplitProcessorTest:
+	def getKeys(self):
+		return lmap(lambda k: ParameterMetadata(k, untracked=True),
+			['DATASETINFO', 'DATASETID', 'DATASETPATH', 'DATASETBLOCK', 'DATASETNICK'])
+
+	def process(self, pNum, splitInfo, result):
+		result.update({
+			'DATASETINFO': '',
+			'DATASETID': splitInfo.get(DataSplitter.DatasetID, None),
+			'DATASETPATH': splitInfo.get(DataSplitter.Dataset, None),
+			'DATASETBLOCK': splitInfo.get(DataSplitter.BlockName, None),
+			'DATASETNICK': splitInfo.get(DataSplitter.Nickname, None),
+			'DATASETSPLIT': pNum,
+		})
+
+def force_intervention():
+	DataParameterSource = ParameterSource.getClass('DataParameterSource')
+	for dp in DataParameterSource.datasetSources:
+		dp.intervention = (set([1]), set([0]), True)
+
+def process_intervention(psource):
+	utils.vprint('')
+	tmp = psource.getJobIntervention()
+	if tmp:
+		if opts.displaymode == 'parseable':
+			utils.vprint('R: %s' % str.join(',', imap(str, tmp[0])))
+			utils.vprint('D: %s' % str.join(',', imap(str, tmp[1])))
+		else:
+			utils.vprint('   Redo: %r' % tmp[0])
+			utils.vprint('Disable: %r' % tmp[1])
+	else:
+		if opts.displaymode == 'parseable':
+			utils.vprint('NOINT')
+		else:
+			utils.vprint('No intervention')
+
+def save_parameters(psource, fn):
+	utils.vprint('')
+	ParameterSource.getClass('GCDumpParameterSource').write(fn, psource)
+	utils.vprint('Parameter information saved to ./%s' % fn)
+
+def setup_config(opts, args):
 	# Set config based on settings from config file or command line
 	configFile = None
 	if os.path.exists(args[0]):
@@ -65,146 +117,114 @@ def main():
 #	configParameters.set('parameter adapter', 'BasicParameterAdapter', '=') # Don't track parameter changes
 	if opts.verbosity > 2:
 		config.changeView(setSections = None).write(sys.stdout)
+	return config
 
-	# Initialize ParameterFactory
-	pm = config.getPlugin('parameter factory', 'SimpleParameterFactory', cls = 'ParameterFactory')
-
-	# Create dataset parameter source
-	class DummySplitter:
-		def getMaxJobs(self):
-			return 3
-		def getSplitInfo(self, pNum):
-			mkEntry = lambda ds, fl, n, nick: { DataSplitter.Dataset: ds, DataSplitter.Nickname: nick,
-				DataSplitter.FileList: fl, DataSplitter.NEntries: n }
-			tmp = [ mkEntry('ds1', ['a', 'b'], 23, 'data_1'), mkEntry('ds1', ['1'], 42, 'data_1'),
-				mkEntry('ds2', ['m', 'n'], 123, 'data_2'), mkEntry('ds2', ['x', 'y', 'z'], 987, 'data_3') ]
-			return tmp[pNum]
-
-	class DataSplitProcessorTest:
-		def getKeys(self):
-			return lmap(lambda k: ParameterMetadata(k, untracked=True),
-				['DATASETINFO', 'DATASETID', 'DATASETPATH', 'DATASETBLOCK', 'DATASETNICK'])
-
-		def process(self, pNum, splitInfo, result):
-			result.update({
-				'DATASETINFO': '',
-				'DATASETID': splitInfo.get(DataSplitter.DatasetID, None),
-				'DATASETPATH': splitInfo.get(DataSplitter.Dataset, None),
-				'DATASETBLOCK': splitInfo.get(DataSplitter.BlockName, None),
-				'DATASETNICK': splitInfo.get(DataSplitter.Nickname, None),
-				'DATASETSPLIT': pNum,
-			})
-
-	if opts.dataset.lower() == 'true':
+def setup_dataset(config, dataset):
+	if dataset.lower() == 'true':
 		utils.vprint('Registering dummy data provider data')
 		dataSplitter = DummySplitter()
-	elif opts.dataset:
-		dataSplitter = DataSplitter.loadState(opts.dataset)
+	else:
+		dataSplitter = DataSplitter.loadState(dataset)
 
+	DataParameterSource = ParameterSource.getClass('DataParameterSource')
+	DataParameterSource.datasetsAvailable['data'] = DataParameterSource(
+		config.getWorkPath(), 'data', None, dataSplitter, DataSplitProcessorTest())
+
+# Initialize ParameterFactory and ParameterSource
+def get_psource(opts, args):
+	config = setup_config(opts, args)
+	pm = config.getPlugin('parameter factory', 'SimpleParameterFactory', cls = 'ParameterFactory')
 	if opts.dataset:
-		DataParameterSource = ParameterSource.getClass('DataParameterSource')
-		DataParameterSource.datasetsAvailable['data'] = DataParameterSource(
-			config.getWorkPath(), 'data', None, dataSplitter, DataSplitProcessorTest())
+		setup_dataset(config, opts.dataset)
+	return pm.getSource(config)
 
-	psource = pm.getSource(config)
+def get_parameters(opts, psource):
+	result = []
+	needGCParam = False
+	if psource.getMaxJobs() is not None:
+		countActive = 0
+		for jobNum in irange(psource.getMaxJobs()):
+			info = psource.getJobInfo(jobNum)
+			if info[ParameterInfo.ACTIVE]:
+				countActive += 1
+			if opts.disabled or info[ParameterInfo.ACTIVE]:
+				if not info[ParameterInfo.ACTIVE]:
+					info['GC_PARAM'] = 'N/A'
+				if str(info['GC_PARAM']) != str(jobNum):
+					needGCParam = True
+				result.append(info)
+		if opts.displaymode == 'parseable':
+			utils.vprint('Count,%d,%d' % (countActive, psource.getMaxJobs()))
+		else:
+			utils.vprint('Number of parameter points: %d' % psource.getMaxJobs())
+			if countActive != psource.getMaxJobs():
+				utils.vprint('Number of active parameter points: %d' % countActive)
+	else:
+		result.append(psource.getJobInfo(123))
+	return (result, needGCParam)
+
+def list_parameters(opts, psource):
+	(result, needGCParam) = get_parameters(opts, psource)
+	enabledOutput = opts.output.split(',')
+	output = lfilter(lambda k: not opts.output or k in enabledOutput, psource.getJobKeys())
+	stored = lfilter(lambda k: k.untracked == False, output)
+	untracked = lfilter(lambda k: k.untracked == True, output)
+
+	if opts.collapse > 0:
+		result_old = result
+		result = {}
+		result_nicks = {}
+		head = [('COLLATE_JOBS', '# of jobs')]
+		if 'DATASETSPLIT' in stored:
+			stored.remove('DATASETSPLIT')
+			if (opts.collapse == 1):
+				stored.append('DATASETNICK')
+				head.append(('DATASETNICK', 'DATASETNICK'))
+			elif opts.collapse == 2:
+				head.append(('COLLATE_NICK', '# of nicks'))
+		for pset in result_old:
+			if ('DATASETSPLIT' in pset) and (opts.collapse == 1):
+				pset.pop('DATASETSPLIT')
+			nickname = None
+			if ('DATASETNICK' in pset) and (opts.collapse == 2):
+				nickname = pset.pop('DATASETNICK')
+			h = md5_hex(repr(lmap(pset.get, stored)))
+			result.setdefault(h, []).append(pset)
+			result_nicks.setdefault(h, set()).add(nickname)
+
+		def doCollate(h):
+			tmp = result[h][0]
+			tmp['COLLATE_JOBS'] = len(result[h])
+			tmp['COLLATE_NICK'] = len(result_nicks[h])
+			return tmp
+		result = lmap(doCollate, result)
+	else:
+		head = [('GC_JOB_ID', '#')]
+		if needGCParam:
+			head.append(('GC_PARAM', 'GC_PARAM'))
+	if opts.active:
+		head.append((ParameterInfo.ACTIVE, 'ACTIVE'))
+	if opts.visible:
+		stored = opts.visible.split(',')
+	head.extend(sorted(izip(stored, stored)))
+	if opts.untracked:
+		head.extend(sorted(imap(lambda n: (n, '(%s)' % n), ifilter(lambda n: n not in ['GC_PARAM', 'GC_JOB_ID'], untracked))))
+	utils.vprint('')
+	utils.printTabular(head, result)
+
+def main():
+	psource = get_psource(opts, args)
+
 	if opts.show_sources:
 		sys.stdout.write(str.join('\n', psource.show()) + '\n\n')
-
-	if opts.force_intervention:
-		DataParameterSource = ParameterSource.getClass('DataParameterSource')
-		for dp in DataParameterSource.datasetSources:
-			dp.intervention = (set([1]), set([0]), True)
-
 	if opts.list_parameters:
-		result = []
-		needGCParam = False
-		if psource.getMaxJobs() is not None:
-			countActive = 0
-			for jobNum in irange(psource.getMaxJobs()):
-				info = psource.getJobInfo(jobNum)
-				if info[ParameterInfo.ACTIVE]:
-					countActive += 1
-				if opts.disabled or info[ParameterInfo.ACTIVE]:
-					if not info[ParameterInfo.ACTIVE]:
-						info['GC_PARAM'] = 'N/A'
-					if str(info['GC_PARAM']) != str(jobNum):
-						needGCParam = True
-					result.append(info)
-			if opts.displaymode == 'parseable':
-				utils.vprint('Count,%d,%d' % (countActive, psource.getMaxJobs()))
-			else:
-				utils.vprint('Number of parameter points: %d' % psource.getMaxJobs())
-				if countActive != psource.getMaxJobs():
-					utils.vprint('Number of active parameter points: %d' % countActive)
-		else:
-			result.append(psource.getJobInfo(123))
-		enabledOutput = opts.output.split(',')
-		output = lfilter(lambda k: not opts.output or k in enabledOutput, psource.getJobKeys())
-		stored = lfilter(lambda k: k.untracked == False, output)
-		untracked = lfilter(lambda k: k.untracked == True, output)
-
-		if opts.collapse > 0:
-			result_old = result
-			result = {}
-			result_nicks = {}
-			head = [('COLLATE_JOBS', '# of jobs')]
-			if 'DATASETSPLIT' in stored:
-				stored.remove('DATASETSPLIT')
-				if (opts.collapse == 1):
-					stored.append('DATASETNICK')
-					head.append(('DATASETNICK', 'DATASETNICK'))
-				elif opts.collapse == 2:
-					head.append(('COLLATE_NICK', '# of nicks'))
-			for pset in result_old:
-				if ('DATASETSPLIT' in pset) and (opts.collapse == 1):
-					pset.pop('DATASETSPLIT')
-				nickname = None
-				if ('DATASETNICK' in pset) and (opts.collapse == 2):
-					nickname = pset.pop('DATASETNICK')
-				h = md5_hex(repr(lmap(pset.get, stored)))
-				result.setdefault(h, []).append(pset)
-				result_nicks.setdefault(h, set()).add(nickname)
-
-			def doCollate(h):
-				tmp = result[h][0]
-				tmp['COLLATE_JOBS'] = len(result[h])
-				tmp['COLLATE_NICK'] = len(result_nicks[h])
-				return tmp
-			result = lmap(doCollate, result)
-		else:
-			head = [('GC_JOB_ID', '#')]
-			if needGCParam:
-				head.append(('GC_PARAM', 'GC_PARAM'))
-		if opts.active:
-			head.append((ParameterInfo.ACTIVE, 'ACTIVE'))
-		if opts.visible:
-			stored = opts.visible.split(',')
-		head.extend(sorted(izip(stored, stored)))
-		if opts.untracked:
-			head.extend(sorted(imap(lambda n: (n, '(%s)' % n), ifilter(lambda n: n not in ['GC_PARAM', 'GC_JOB_ID'], untracked))))
-		utils.vprint('')
-		utils.printTabular(head, result)
-
-	if opts.save:
-		utils.vprint('')
-		ParameterSource.getClass('GCDumpParameterSource').write(opts.save, psource)
-		utils.vprint('Parameter information saved to ./%s' % opts.save)
-
+		list_parameters(opts, psource)
+	if opts.force_intervention:
+		force_intervention()
 	if opts.intervention:
-		utils.vprint('')
-		tmp = psource.getJobIntervention()
-		if tmp:
-			if opts.displaymode == 'parseable':
-				utils.vprint('R: %s' % str.join(',', imap(str, tmp[0])))
-				utils.vprint('D: %s' % str.join(',', imap(str, tmp[1])))
-			else:
-				utils.vprint('   Redo: %r' % tmp[0])
-				utils.vprint('Disable: %r' % tmp[1])
-		else:
-			if opts.displaymode == 'parseable':
-				utils.vprint('NOINT')
-			else:
-				utils.vprint('No intervention')
+		process_intervention(psource)
+	if opts.save:
+		save_parameters(psource, opts.save)
 
 if __name__ == '__main__':
 	sys.exit(main())
