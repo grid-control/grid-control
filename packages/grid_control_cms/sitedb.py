f198d3cd68467b7e56167c3ef2732f1cb2b4452e
Fred Stober <stober@cern.ch>
2016-06-07 17:14:37 +0000

    refactored SiteDB
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1897 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- packages/grid_control_cms/sitedb.py
+++ packages/grid_control_cms/sitedb.py
@@ -12,55 +12,36 @@
 # | See the License for the specific language governing permissions and
 # | limitations under the License.
 
-import re, logging
+import re
 from grid_control.utils.webservice import GridJSONRestClient
 from python_compat import ifilter, imap, izip, lmap, set
 
-def unflatten_json(data):
-	"""Tranform input to unflatten JSON format"""
-	columns = data['desc']['columns']
-	for row in data['result']:
-		yield dict(izip(columns, row))
-
-
 class SiteDB(object):
 	def __init__(self, url = None):
-		self._gjrc = GridJSONRestClient(url or 'https://cmsweb.cern.ch/sitedb/data/prod/', 'VOMS proxy needed to query siteDB!')
+		self._gjrc = GridJSONRestClient(url or 'https://cmsweb.cern.ch/sitedb/data/prod', 'VOMS proxy needed to query siteDB!')
 
-	def _query(self, api, match = None):
-		params = None
-		if match:
-			params = {'match': match}
-		return unflatten_json(self._gjrc.get(api = api, params = params))
-
-	def _people(self, username = None):
-		return self._query('people', username)
-
-	def _site_names(self, site_name = None):
-		return self._query('site-names', site_name)
-
-	def _site_resources(self):
-		return self._query('site-resources')
+	def _query(self, api, **kwargs):
+		data = self._gjrc.get(api = api, params = kwargs or None)
+		columns = data['desc']['columns']
+		for row in data['result']:
+			yield dict(izip(columns, row))
 
 	def cms_name_to_se(self, cms_name):
-		cms_name = cms_name.replace('*', '.*')
-		cms_name = cms_name.replace('%', '.*')
-		cms_name_regex = re.compile(cms_name)
-
-		psn_site_names = ifilter(lambda site: site['type'] == 'psn' and cms_name_regex.match(site['alias']), self._site_names())
+		cms_name_regex = re.compile(cms_name.replace('*', '.*').replace('%', '.*'))
+		psn_site_names = ifilter(lambda site: site['type'] == 'psn' and cms_name_regex.match(site['alias']), self._query('site-names'))
 		site_names = set(imap(lambda x: x['site_name'], psn_site_names))
-		return lmap(lambda x: x['fqdn'], ifilter(lambda x: (x['type'] == 'SE') and (x['site_name'] in site_names), self._site_resources()))
+		return lmap(lambda x: x['fqdn'], ifilter(lambda x: (x['type'] == 'SE') and (x['site_name'] in site_names), self._query('site-resources')))
 
 	def se_to_cms_name(self, se):
 		site_names = []
-		for site_resource in ifilter(lambda resources: resources['fqdn'] == se, self._site_resources()):
-			site_names.extend(self._site_names(site_name=site_resource['site_name']))
+		for site_resource in ifilter(lambda resources: resources['fqdn'] == se, self._query('site-resources')):
+			site_names.extend(self._query('site-names', match = site_resource['site_name']))
 		return lmap(lambda x: x['alias'], ifilter(lambda site: site['type'] == 'cms', site_names))
 
 	def dn_to_username(self, dn):
-		for user in ifilter(lambda this_user: this_user['dn'] == dn, self._people()):
+		for user in ifilter(lambda this_user: this_user['dn'] == dn, self._query('people')):
 			return user['username']
 
 	def username_to_dn(self, username):
-		for user in self._people(username = username):
+		for user in self._query('people', match = username):
 			return user['dn']
