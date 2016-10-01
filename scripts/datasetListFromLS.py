216774f366b455de6f293fd868976b444fb35e62
Fred Stober <stober@cern.ch>
2016-10-01 20:40:56 +0000

    datasetListFromLS.py: Added option to recurse into local directories
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control@1948 29098800-e21f-4f28-a8ff-84ee76f0ac90

--- scripts/datasetListFromLS.py
+++ scripts/datasetListFromLS.py
@@ -18,7 +18,10 @@ from datasetListFromX import addDatasetListOptions, discoverDataset
 from gcSupport import Options, scriptOptions
 
 parser = Options(usage = '%s [OPTIONS] <data path> <dataset name> <pattern (*.root) / files>')
-parser.addText(None, 'p', 'path', dest = 'dataset', default = '.', help = 'Path to dataset files')
+parser.addText(None, 'p', 'path',    dest = 'dataset',        default = '.',
+	help = 'Path to dataset files')
+parser.addBool(None, 'r', 'recurse', dest = 'source recurse', default = False,
+	help = 'Recurse into subdirectories if supported')
 addDatasetListOptions(parser)
 options = scriptOptions(parser, arg_keys = ['dataset', 'dataset name pattern', 'filename filter'])
 logging.getLogger('user').setLevel(logging.CRITICAL)
