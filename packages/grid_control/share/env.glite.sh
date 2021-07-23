e945c542ac43295440335bf02f36e11de57d2a6a
johannes-lange <johannes.lange@rwth-aachen.de>
2021-07-23 12:29:04 +0200

    proper "latest" grid ui paths

--- packages/grid_control/share/env.glite.sh
+++ packages/grid_control/share/env.glite.sh
@@ -67,9 +67,9 @@ if [ -z "$GLITE_LOCATION" ] && [ -d "$GLITE_LOCATION" ]; then
         GC_GLITE_TYPE="LOCAL"
 elif gc_find_grid "USER" "$GC_GLITE_LOCATION"; then
 	GC_GLITE_TYPE="USER"
-elif [[ $(uname -a) == *"el7"* ]] &&gc_find_grid "CVMFS (el7)" "/cvmfs/grid.cern.ch/centos7-ui-preview-v01/etc/profile.d/setup-c7-ui-example.sh"; then
+elif [[ $(uname -a) == *"el7"* ]] &&gc_find_grid "CVMFS (el7)" "/cvmfs/grid.cern.ch/umd-c7ui-latest/etc/profile.d/setup-c7-ui-example.sh"; then
 	GC_GLITE_TYPE="CVMFS"
-elif gc_find_grid "CVMFS" "/cvmfs/grid.cern.ch/emi3ui-latest/etc/profile.d/setup-ui-example.sh"; then
+elif gc_find_grid "CVMFS" "/cvmfs/grid.cern.ch/umd-sl6ui-latest/etc/profile.d/setup-ui-example.sh"; then
 	GC_GLITE_TYPE="CVMFS"
 elif gc_find_grid "CVMFS - 2nd try" $(ls -1t /cvmfs/grid.cern.ch/*/etc/profile.d/grid*.sh 2> /dev/null | head -n 1); then
 	GC_GLITE_TYPE="CVMFS-2"
